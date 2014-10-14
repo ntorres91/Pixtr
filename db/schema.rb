8f6f8fb42b9a33a7eab89b80fdfa822db6d0a099
ntorres91 <neil.k.torres@gmail.com>
2014-10-14 13:15:04 -0400

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009215703) do
+ActiveRecord::Schema.define(version: 20141013224143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "body"
+    t.integer  "image_id"
+  end
 
   create_table "galleries", force: true do |t|
     t.string   "name"
@@ -24,8 +31,12 @@ ActiveRecord::Schema.define(version: 20141009215703) do
   end
 
   create_table "images", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "gallery_id"
   end
 
 end
